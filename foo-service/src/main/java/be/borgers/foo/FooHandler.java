a5bfefb0e72b9ac8b9d7822e72dc4909795bd486
Toon Borgers <borgers.toon@gmail.com>
2018-05-24 00:54:32 +0200

    Call bar service from foo service

--- foo-service/src/main/java/be/borgers/foo/FooHandler.java
+++ foo-service/src/main/java/be/borgers/foo/FooHandler.java
@@ -1,16 +1,29 @@
 package be.borgers.foo;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
-import static org.springframework.web.reactive.function.BodyInserters.fromObject;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @Component
 public class FooHandler {
-    public Mono<ServerResponse> handle(ServerRequest request) {
-        return ok().body(fromObject("Foo"));
+    private final WebClient webclient;
+
+    public FooHandler() {
+        webclient = WebClient.create("http://bar-service:8080");
+    }
+
+    Mono<ServerResponse> handle(ServerRequest request) {
+        Mono<String> rMono = webclient.method(HttpMethod.GET)
+                .uri("/bar")
+                .exchange()
+                .map(response -> response.bodyToMono(String.class))
+                .flatMap(stringMono -> stringMono.map(s -> "Foo " + s));
+
+        return ok().body(rMono, String.class);
     }
 }
