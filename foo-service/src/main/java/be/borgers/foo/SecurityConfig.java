7bbf8d94ddd94cd3fe1b27e282ccf65b9e0b4f39
Toon Borgers <borgers.toon@gmail.com>
2018-05-24 06:35:15 +0200

    Clean up code

--- foo-service/src/main/java/be/borgers/foo/SecurityConfig.java
+++ foo-service/src/main/java/be/borgers/foo/SecurityConfig.java
@@ -30,11 +30,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 );
     }
 
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .antMatchers("*/**")
-//                .authenticated();
-//    }
 }
