f7583e293186523147b0b3ac80fce6bb9fec0e6a
Toon Borgers <borgers.toon@gmail.com>
2018-05-24 02:31:06 +0200

    Show which host the bar service was run on

--- bar-service/src/main/kotlin/be/borgers/bar/BarHandler.kt
+++ bar-service/src/main/kotlin/be/borgers/bar/BarHandler.kt
@@ -1,12 +1,13 @@
 package be.borgers.bar
 
 import org.springframework.stereotype.Component
-import org.springframework.web.reactive.function.BodyInserters
+import org.springframework.web.reactive.function.BodyInserters.fromObject
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.ServerResponse.ok
 import reactor.core.publisher.Mono
 
 @Component
 class BarHandler {
-    fun handle(req: ServerRequest): Mono<ServerResponse> = ServerResponse.ok().body(BodyInserters.fromObject("Bar"))
+    fun handle(req: ServerRequest): Mono<ServerResponse> = ok().body(fromObject("Bar from " + System.getenv("HOSTNAME")))
 }
