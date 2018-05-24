97ce1defc0276143198580803cd737606e67d123
Toon Borgers <borgers.toon@gmail.com>
2018-05-24 06:34:22 +0200

    Configure basic security using k8s secret

--- foo-service/src/main/java/be/borgers/foo/SecurityConfig.java
+++ foo-service/src/main/java/be/borgers/foo/SecurityConfig.java
@@ -1,4 +1,40 @@
 package be.borgers.foo;
 
-public class SecurityConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.springframework.security.core.userdetails.User.builder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Value("${security.user}")
+    private String user;
+    @Value("${security.password}")
+    private String password;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser(
+                        builder()
+                                .passwordEncoder(encoder::encode)
+                                .username(user)
+                                .password(password)
+                                .roles("USER")
+                );
+    }
+
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("*/**")
+//                .authenticated();
+//    }
 }
